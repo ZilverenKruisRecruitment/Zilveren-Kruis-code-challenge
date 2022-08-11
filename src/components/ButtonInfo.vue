b7371a00237330b5ded4fa5838bd639e1a7f04f4
Gaby Khoe <gaby@code.nl>
2022-08-11 12:16:28 +0200

    Make vue components

---
+++ src/components/ButtonInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <button id="info-phone" type="button" class="btn popover__btn btn--popover btn-none">
+    <span class="sr-only">
+      Informatie weergeven
+    </span>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: 'ButtonInfo'
+  }
+</script>
