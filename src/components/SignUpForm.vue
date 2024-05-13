a654da18efc515c9479a44f26ffa7d665fe33566
swu1337 <stevenwuwastaken@gmail.com>
2024-05-13 23:04:27 +0200

    V-modeling forms

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -9,19 +9,10 @@
                 <div class="form-group">
                     <h3>Reden van aanmelding</h3>
                     <div class="form-input my-4">
-                        <label id="aanmeldreden-label" class="input__title">
-                            Wat is de reden van uw aanvraag?
-                        </label>
+                        <label id="aanmeldreden-label" class="input__title">Wat is de reden van uw aanvraag?</label>
                         <div class="input__group">
-                            <select class="form-control">
-                                <option>
-                                    Nieuwe werkgever met collectiviteit bij
-                                    Zilveren Kruis
-                                </option>
-                                <option selected>
-                                    Overstappen per 1-1-2023 naar Zilveren
-                                    Kruis
-                                </option>
+                            <select class="form-control" v-model="userInput.reason">
+                                <option v-for="option in formOptions.reasons" :key="option.value" :value="option.value" :disabled="option.disabled">{{ option.label }}</option>
                             </select>
                         </div>
                     </div>
@@ -34,17 +25,17 @@
                             <input
                                 class="input__field form-control"
                                 type="text"
+                                v-model="userInput.firstName"
                             />
                         </div>
                     </div>
                     <div class="form-input my-4">
                         <div class="input__group">
-                            <label class="input__title">
-                                Tussenvoegsels
-                            </label>
+                            <label class="input__title">Tussenvoegsels</label>
                             <input
                                 class="input__field form-control"
                                 type="text"
+                                v-model="userInput.surName"
                             />
                         </div>
                     </div>
@@ -54,6 +45,7 @@
                             <input
                                 class="input__field form-control"
                                 type="text"
+                                v-model="userInput.lastName"
                             />
                         </div>
                     </div>
@@ -69,6 +61,8 @@
                                         class="radio__input custom-control-input"
                                         type="radio"
                                         name="geslacht"
+                                        value="Man"
+                                        v-model="userInput.gender"
                                     />
                                     <label
                                         class="radio__label custom-control-label"
@@ -85,6 +79,8 @@
                                         class="radio__input custom-control-input"
                                         type="radio"
                                         name="geslacht"
+                                        value="Vrouw"
+                                        v-model="userInput.gender"
                                     />
                                     <label
                                         class="radio__label custom-control-label"
@@ -98,35 +94,26 @@
                     </div>
                     <div class="form-input my-4">
                         <div class="input__group">
-                            <label class="input__title">
-                                Geboortedatum
-                            </label>
-                            <!-- <input
-                                class="input__field form-control"
-                                type="text"
-                            /> -->
-                            <VueDatePicker v-model="date"></VueDatePicker>
+                            <label class="input__title">Geboortedatum</label>
+                            <VueDatePicker format="dd/MM/yyyy" :enable-time-picker="false" v-model="userInput.birthday"></VueDatePicker>
                         </div>
                     </div>
                     <div class="form-input my-4">
                         <div class="input__group">
-                            <label class="input__title">
-                                Burgerservicenummer
-                            </label>
+                            <label class="input__title">Burgerservicenummer</label>
                             <input
-                                class="input__field form-control is-invalid"
+                                class="input__field form-control"
+                                :class="!isValidBSN(userInput.bsn) && 'is-invalid'"
                                 type="text"
+                                v-model="userInput.bsn"
                             />
                         </div>
                         <div
                             class="input__feedback invalid-feedback mt-1"
                             aria-live="polite"
+                            v-if="!isValidBSN(userInput.bsn)"
                         >
-                            <span
-                                >Helaas is het ingevoerde
-                                burgerservicenummer niet geldig. Probeer het
-                                opnieuw.</span
-                            >
+                            <span>Helaas is het ingevoerde burgerservicenummer niet geldig. Probeer het opnieuw.</span>
                         </div>
                     </div>
                 </div>
@@ -141,14 +128,31 @@
                         zit en dit is dus bij elke verzekeraar hetzelfde.
                     </p>
                     <div class="input__group">
-                        <label class="input__title">
-                            Kies uw basisverzekering
-                        </label>
+                        <label class="input__title">Kies uw basisverzekering</label>
                         <div class="form-row">
-                            <div class="radio__tile">
+                            <div class="radio__tile" v-for="option in formOptions.insurances" :key="option.value">
                                 <div
                                     class="radio custom-radio radio__option"
-                                >
+                                    :class="userInput.insurancePackage === option.value && 'radio__tile--choice'">
+                                    <input
+                                        :value="option.value"
+                                        type="radio"
+                                        name="radio-insurance"
+                                        :id="option.id"
+                                        class="radio__input custom-control-input"
+                                        v-model="userInput.insurancePackage"
+                                    />
+                                    <label
+                                        :for="option.id"
+                                        class="radio__label custom-control-label"
+                                    >
+                                        <p class="radio__description">{{ option.label }}</p>
+                                        <p class="radio__price">&euro; {{ formatPrice(option.price) }} per maand</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="radio__tile">
+                                <div class="radio custom-radio radio__option">
                                     <input
                                         value="Basis Budget"
                                         type="radio"
@@ -160,22 +164,14 @@
                                         for="radio-insurance-basis-budget"
                                         class="radio__label custom-control-label"
                                     >
-                                        <p class="radio__description">
-                                            Basis Budget
-                                        </p>
-                                        <p class="radio__price">
-                                            € 1.393,26 per jaar
-                                        </p>
+                                        <p class="radio__description">Basis Budget</p>
+                                        <p class="radio__price">&euro; 1.393,26 per jaar</p>
                                     </label>
                                 </div>
                             </div>
                             <div class="radio__tile radio__tile--choice">
-                                <div class="badge radio__tile-badge">
-                                    Meest gekozen
-                                </div>
-                                <div
-                                    class="radio custom-radio radio__option"
-                                >
+                                <div class="badge radio__tile-badge">Meest gekozen</div>
+                                <div class="radio custom-radio radio__option">
                                     <input
                                         value="Basis Zeker"
                                         type="radio"
@@ -187,12 +183,8 @@
                                         for="radio-insurance-basis-zeker"
                                         class="radio__label custom-control-label"
                                     >
-                                        <p class="radio__description">
-                                            Basis Zeker
-                                        </p>
-                                        <p class="radio__price">
-                                            € 1.483,54 per jaar
-                                        </p>
+                                        <p class="radio__description">Basis Zeker</p>
+                                        <p class="radio__price">&euro; 1.483,54 per jaar</p>
                                     </label>
                                 </div>
                             </div>
@@ -211,12 +203,8 @@
                                         for="radio-insurance-basis-exclusief-(restitutie)"
                                         class="radio__label custom-control-label"
                                     >
-                                        <p class="radio__description">
-                                            Basis Exclusief (Restitutie)
-                                        </p>
-                                        <p class="radio__price">
-                                            € 1.624,62 per jaar
-                                        </p>
+                                        <p class="radio__description">Basis Exclusief (Restitutie)</p>
+                                        <p class="radio__price">&euro; 1.624,62 per jaar</p>
                                     </label>
                                 </div>
                             </div>
@@ -358,9 +346,96 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css'
+    import { ref } from 'vue';
+    import VueDatePicker from '@vuepic/vue-datepicker';
+    import '@vuepic/vue-datepicker/dist/main.css'
 
-const date = ref();
+    const formOptions = {
+        reasons: [
+            {
+                label: 'Er is nog niets geselecteerd',
+                value: '',
+                disabled: true,
+            },
+            {
+                label: 'Nieuwe werkgever met collectiviteit bij Zilveren Kruis',
+                value: 'Nieuwe werkgever met collectiviteit bij Zilveren Kruis'
+            },
+            {
+                label: 'Overstappen per 1-1-2023 naar Zilveren Kruis',
+                value: 'Overstappen per 1-1-2023 naar Zilveren Kruis'
+            },
+        ],
+        insurances: [
+            {
+                id: 'radio-insurance-basis-budget',
+                label: 'Basis Budget',
+                price: 125,
+                value: 'Basis Budget',
+            },
+            {
+                id: 'radio-insurance-basis-zeker',
+                label: 'Basis Zeker',
+                price: 130,
+                value: 'Basis Zeker',
+            },
+            {
+                id: 'radio-insurance-basis-exclusief-(restitutie)',
+                label: 'Basis Exclusief (Restitutie)',
+                price: 150,
+                value: 'Basis Exclusief (Restitutie)',
+            },
+        ]
+
+    }
+
+    const userInput = ref({
+        reason: '',
+        firstName: '',
+        surName: '',
+        lastName: '',
+        gender: '',
+        birthday: '',
+        bsn: '',
+        insurancePackage: 'Basis Zeker'
+    });
+
+    console.log('hello script setup')
+    console.log(userInput.value);
+
+    //src: https://github.com/willemverbuyst/bsn-js/blob/main/src/validator.ts
+    const isValidBSN = (bsn) => {
+	    if (bsn.length < 8 || bsn.length > 9) return false
+
+        const numbers = Array.from(String(bsn), Number)
+        const lastNumber = numbers[numbers.length - 1]
+
+        if (typeof lastNumber === 'number' && !numbers.includes(NaN)) {
+            return (
+                (numbers
+                    .slice(0, -1)
+                    .reduce((a, b, i, arr) => b * (arr.length + 1 - i) + a, 0) -
+                    lastNumber) %
+                    11 ===
+                0
+            )
+        }
+
+        return false
+    }
+
+    //src: ChatGPT
+    const formatPrice = price =>  {
+    // Convert price to a string and split it into integer and decimal parts
+    let [integerPart, decimalPart] = price.toFixed(2).toString().split(".");
+
+    // Add comma for thousands separator
+    integerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+    // Concatenate integer and decimal parts with comma
+    return integerPart + "," + decimalPart;
+}
+
+// Example usage
+console.log(formatPrice(10000025.89)); // Output: 1.250,00
 </script>
